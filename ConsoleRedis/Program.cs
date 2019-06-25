b2b24f75988a170b1916b8098b29fc4bd4511ccc
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2019-06-25 10:13:07 +0200

    First commit

---
+++ ConsoleRedis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace ConsoleRedis
+{
+    // docker run -d -p 6379:6379 redis
+    // docker exec -it 666333e4ad18 redis-cli
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+            IDatabase db = redis.GetDatabase();
+
+            string value = db.StringGet("name");
+            Console.WriteLine(value);
+
+            Console.ReadKey(); 
+        }
+    }
+}
